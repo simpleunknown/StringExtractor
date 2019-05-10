122c6215d3940c0029664178c7be286c7bdef3b3
simpleunknown <50069880+simpleunknown@users.noreply.github.com>
2019-05-10 23:40:04 +0530

    Whole Code
    this is full code unzipped

---
+++ DLLStringExtractor/DLLStringExtractor/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FolderBrowserDialogSampleInCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowseFolderButton = new System.Windows.Forms.Button();
+            this.filebutton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.GenStrings = new System.Windows.Forms.Button();
+            this.explist = new System.Windows.Forms.Button();
+            this.Dictionary = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BrowseFolderButton
+            // 
+            this.BrowseFolderButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BrowseFolderButton.ForeColor = System.Drawing.Color.Green;
+            this.BrowseFolderButton.Location = new System.Drawing.Point(137, 75);
+            this.BrowseFolderButton.Name = "BrowseFolderButton";
+            this.BrowseFolderButton.Size = new System.Drawing.Size(98, 28);
+            this.BrowseFolderButton.TabIndex = 0;
+            this.BrowseFolderButton.Text = "Browse Folder containing DLL";
+            this.BrowseFolderButton.UseVisualStyleBackColor = false;
+            this.BrowseFolderButton.Click += new System.EventHandler(this.BrowseFolderButton_Click);
+            // 
+            // filebutton
+            // 
+            this.filebutton.BackColor = System.Drawing.Color.Black;
+            this.filebutton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.filebutton.Location = new System.Drawing.Point(402, 75);
+            this.filebutton.Name = "filebutton";
+            this.filebutton.Size = new System.Drawing.Size(98, 28);
+            this.filebutton.TabIndex = 2;
+            this.filebutton.Text = "Open File containing DLL";
+            this.filebutton.UseVisualStyleBackColor = false;
+            this.filebutton.Click += new System.EventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Location = new System.Drawing.Point(80, 124);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(500, 193);
+            this.textBox2.TabIndex = 3;
+            // 
+            // GenStrings
+            // 
+            this.GenStrings.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.GenStrings.ForeColor = System.Drawing.Color.Aqua;
+            this.GenStrings.Location = new System.Drawing.Point(252, 355);
+            this.GenStrings.Name = "GenStrings";
+            this.GenStrings.Size = new System.Drawing.Size(186, 46);
+            this.GenStrings.TabIndex = 4;
+            this.GenStrings.Text = "Generate Strings";
+            this.GenStrings.UseVisualStyleBackColor = false;
+            this.GenStrings.Click += new System.EventHandler(this.GenStrings_click);
+            // 
+            // explist
+            // 
+            this.explist.Location = new System.Drawing.Point(619, 200);
+            this.explist.Name = "explist";
+            this.explist.Size = new System.Drawing.Size(75, 23);
+            this.explist.TabIndex = 5;
+            this.explist.Text = "Export_list";
+            this.explist.UseVisualStyleBackColor = true;
+            this.explist.Click += new System.EventHandler(this.explist_Click);
+            // 
+            // Dictionary
+            // 
+            this.Dictionary.Location = new System.Drawing.Point(486, 355);
+            this.Dictionary.Name = "Dictionary";
+            this.Dictionary.Size = new System.Drawing.Size(133, 45);
+            this.Dictionary.TabIndex = 6;
+            this.Dictionary.Text = "Dictionary File";
+            this.Dictionary.UseVisualStyleBackColor = true;
+            this.Dictionary.Click += new System.EventHandler(this.Dict_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 522);
+            this.Controls.Add(this.Dictionary);
+            this.Controls.Add(this.explist);
+            this.Controls.Add(this.GenStrings);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.filebutton);
+            this.Controls.Add(this.BrowseFolderButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BrowseFolderButton;
+        private System.Windows.Forms.Button filebutton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button GenStrings;
+        private System.Windows.Forms.Button explist;
+        private System.Windows.Forms.Button Dictionary;
+    }
+}
+
---
+++ DLLStringExtractor/DLLStringExtractor/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+
+namespace FolderBrowserDialogSampleInCSharp
+{
+   
+    public partial class Form1 : Form
+    {
+        OpenFileDialog fileDlg;
+        OpenFileDialog fileDlgDict;
+        OpenFileDialog fileDlg2;
+       string a;
+        FileInfo[] Files;
+        FileInfo[] Files2;
+        FolderBrowserDialog folderBrowserDialog3save = new FolderBrowserDialog();
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+        string strCmdText, DstrCmdText;
+        string dict;
+
+        //startInfo.FileName = "cmd.exe";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseFolderButton_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+           
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                DirectoryInfo d = new DirectoryInfo(folderDlg.SelectedPath);
+                FileInfo[] Files = d.GetFiles("*.dll"); 
+                foreach (FileInfo filen in Files)
+                {
+                    textBox2.AppendText(filen.ToString());
+                    textBox2.AppendText("\r\n");
+                }
+                 Environment.SpecialFolder root = folderDlg.RootFolder;
+                Files2= Files;
+            }
+
+        }
+        
+
+        public void openFileDialog1_FileOk(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+           OpenFileDialog fileDlg = new OpenFileDialog();          
+           
+            fileDlg.Multiselect = true;
+            DialogResult result = fileDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                foreach (String file in fileDlg.SafeFileNames)
+                { 
+                    textBox2.AppendText(file);
+                    textBox2.AppendText("\r\n");
+                }
+               
+            }
+            fileDlg2 = fileDlg;
+        }
+
+        private void GenStrings_click(object sender,EventArgs e)
+        {
+            if (fileDlg2 != null)
+            {
+                foreach (string lined in fileDlg2.FileNames)
+                {
+                    foreach (string line in lined.Split(new[] { '\n' }))
+                    {
+                        //string newline = line.Replace(@"\\", @"\");
+                        
+                   
+                        //strCmdText = "/C" + " " + "strings" + " " + line + " " + ">" + line + ".txt";
+                        //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+                        // code for searching using dict file 
+                        ProcessStartInfo startinfo = new ProcessStartInfo();
+                        Process cmd = new Process();
+                        //this is forst repo
+                        if (dict != null)
+
+                        { strCmdText = "/C" + " " + "strings" + " \"" + line + "\" " + "|" + " " + "findstr" + " " + "/G:" + "\"" + a + "\""; }
+                        else if (dict == null)
+                        { strCmdText = "/C" + " " + "strings" + " \"" + line + "\""; }
+                        //strCmdText = "/C" +" "+"dir" +" " + fileDlg2.FileName.Replace(fileDlg2.SafeFileName,"") ;
+                        cmd.StartInfo.WorkingDirectory = @"C:\Windows\System32";
+                        cmd.StartInfo.FileName = "cmd.exe";
+                        cmd.StartInfo.Arguments = strCmdText.ToString();
+                        cmd.StartInfo.UseShellExecute = false;
+                       cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        cmd.StartInfo.RedirectStandardOutput = true;
+                        cmd.Start();
+                        cmd.WaitForExit();
+                        string output = cmd.StandardOutput.ReadToEnd();
+                      string savefile= Directory.GetCurrentDirectory();
+                        savefile += @"\";
+                        string nonextsavefile= Path.GetFileNameWithoutExtension(line);
+                        if (dict != null)
+
+                        { savefile += nonextsavefile+"_Dictonary" + ".txt";  }
+                        else if (dict == null)
+                        { savefile += nonextsavefile + ".txt"; }
+                        
+                            File.WriteAllText(savefile, output);
+                     
+                        
+                        
+
+                    }
+                }
+            }
+            else if(Files2!=null)
+            {
+                foreach (FileInfo filene in Files2)
+                {
+                    //old code
+
+                    //strCmdText = "/C" + " " + "strings" + " \"" + filene.FullName.ToString() + "\" " + ">" + filene.FullName.ToString() +"__folder"+ ".txt";
+                    //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+                    ProcessStartInfo startinfo = new ProcessStartInfo();
+                    Process cmd = new Process();
+                    if (dict != null)
+                    {
+                        strCmdText = "/C" + " " + "strings" + " \"" + filene.FullName.ToString() + "\"" + "|" + " " + "findstr" + " " + "/G:" + "\"" + a + "\"";
+                    }
+                    else if (dict == null)
+                    {
+                        strCmdText = "/C" + " " + "strings" + " \"" + filene.FullName.ToString() + "\"";
+                    }
+                    cmd.StartInfo.WorkingDirectory = @"C:\Windows\System32";
+                    cmd.StartInfo.FileName = "cmd.exe";
+                    cmd.StartInfo.Arguments = strCmdText.ToString();
+                    cmd.StartInfo.UseShellExecute = false;
+                    cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    cmd.StartInfo.RedirectStandardOutput = true;
+                    cmd.Start();
+                    cmd.WaitForExit();
+                    string output = cmd.StandardOutput.ReadToEnd();
+                    string savefile = Directory.GetCurrentDirectory();
+                    savefile += @"\";
+                    string nonextsavefile = Path.GetFileNameWithoutExtension(filene.ToString());
+                    if (dict != null)
+
+                    { savefile += nonextsavefile + "_Dictonary" + ".txt"; }
+                    else if (dict == null)
+                    { savefile += nonextsavefile + ".txt"; }
+                    File.WriteAllText(savefile, output);
+
+
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("ERROR select any option");
+            }
+
+            dict = null;
+        }
+
+        private void explist_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog3save.ShowDialog() == DialogResult.OK)
+            {
+                // create a writer and open the file
+               
+                // write a line of text to the file
+                File.WriteAllText(folderBrowserDialog3save.SelectedPath + "DLLnames.txt", textBox2.Text);
+                
+                // close the stream
+                
+               // MessageBox.Show("Saved to " + folderBrowserDialog3save.SelectedPath + "\\logfile.txt", "Saved Log File", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public void Dict_Click(object sender, EventArgs e)
+        {
+             dict="inid";
+            
+            //textBox2.Clear();
+            OpenFileDialog fileDlgDict = new OpenFileDialog();
+            DialogResult result = fileDlgDict.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                
+                     a = fileDlgDict.FileName;
+                a.Replace(@"\\", @"\");
+                dict = "Yes";
+               // a.ToString();
+                
+
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
